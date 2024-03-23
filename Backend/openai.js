3c3206681a18694e605b3370ef7b9029d628efeb
EthanHaller <ttk4ey@virginia.edu>
2024-03-23 13:25:53 -0400

    finalize openai api call to summarize html content

--- Backend/openai.js
+++ Backend/openai.js
@@ -2,26 +2,25 @@ const OpenAI = require("openai")
 
 async function summarize(html) {
 	const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY })
-    const today = new Date()
 
 	const completion = await openai.chat.completions.create({
 		messages: [
 			{
 				role: "system",
 				content: `
-				Tell me the title of this article. Summarize it and give me some key points.
-
-				Output your message in the following JSON format:
-				
-				{
-				  title: "",
-				  summary: "",
-				  key-points: [
-					point: "",
-					point: "",
-					...
-				  ]
-				}`,
+					Tell me the title of this article from the body of the HTML. Summarize the article and give me some detailed key points for studying.
+	
+					Output your message in the following JSON format:
+					
+					{
+					  title: "",
+					  summary: "",
+					  key-points: [
+						point: "",
+						point: "",
+						...
+					  ]
+					}`,
 			},
 			{
 				role: "user",
@@ -31,9 +30,12 @@ async function summarize(html) {
 		model: "gpt-3.5-turbo",
 	})
 
-    let message = await JSON.parse(completion.choices[0].message.content)
-	return message
+	let message = completion.choices[0].message.content
+	const startIdx = message.indexOf("{")
+	const endIdx = message.lastIndexOf("}")
+	message = message.slice(startIdx, endIdx + 1)
+	messageJSON = await JSON.parse(message)
+	return messageJSON
 }
-
 
 module.exports = { summarize }
