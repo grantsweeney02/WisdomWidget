87e714eedc6565634a2e5e916f817ddd1b57f624
EthanHaller <ttk4ey@virginia.edu>
2024-03-23 14:21:46 -0400

    begin formatting

--- Dashboard/src/components/DashboardContent.jsx
+++ Dashboard/src/components/DashboardContent.jsx
@@ -1,35 +1,39 @@
-import Citations from "./Citations";
+import Citations from "./Citations"
 
 const DashboardContent = ({ assignment }) => {
-    const getAllNoteUrls = (notes) => {
-        const urls = [];
-        notes.forEach((note) => {
-            urls.push(note.url);
-        });
-        return urls;
-    };
+	const getAllNoteUrls = (notes) => {
+		const urls = []
+		notes.forEach((note) => {
+			urls.push(note.url)
+		})
+		return urls
+	}
 
-    return (
-        <div>
-            <h1>{assignment.name}</h1>
-            {assignment.due ? <h4>Due Date: {assignment.due}</h4> : null}
-            <h4>Notes: </h4>
-            {assignment.notes.map((note, index) => (
-                <p key={index}>{note.name}</p>
-            ))}
-            <Citations urls={getAllNoteUrls(assignment.notes)} />
-            <h4>More Resources: </h4>
-            <ul>
-                {assignment.urlRecs.map((url, index) => (
-                    <li key={index}>
-                        <a key={index} href={url}>
-                            {url}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+	return (
+		<>
+			{assignment && (
+				<div className="col-10">
+					<h1>{assignment.name}</h1>
+					{assignment.due ? <h4>Due Date: {assignment.due}</h4> : null}
+					<h4>Notes: </h4>
+					{assignment.notes.map((note, index) => (
+						<p key={index}>{note.name}</p>
+					))}
+					<Citations urls={getAllNoteUrls(assignment.notes)} />
+					<h4>More Resources: </h4>
+					<ul>
+						{assignment.urlRecs.map((url, index) => (
+							<li key={index}>
+								<a key={index} href={url}>
+									{url}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</>
+	)
+}
 
-export default DashboardContent;
+export default DashboardContent
