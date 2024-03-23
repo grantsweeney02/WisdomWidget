a1b5215076a5b54a82cb4d01ede0f475ea3e5bed
simonanderson16 <uhx8bu@virginia.edu>
2024-03-23 17:59:22 -0400

    logout logic

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -1,31 +1,49 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Dashboard from "./components/Dashboard"
-import dummyUser from "../dummyUser.json"
-import NoteDetails from "./components/NoteDetails"
-import ClassesSidebar from "./components/ClassesSidebar"
-import AssignmentDetails from "./components/AssignmentDetails"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import dummyUser from "../dummyUser.json";
+import NoteDetails from "./components/NoteDetails";
+import ClassesSidebar from "./components/ClassesSidebar";
+import AssignmentDetails from "./components/AssignmentDetails";
+import { auth } from "../firebaseConfig";
 
-import "./styles/theme.css"
+import "./styles/theme.css";
+import { useEffect, useState } from "react";
 
 function App() {
-	const loggedIn = true
+    const [loggedIn, setLoggedIn] = useState(auth.currentUser);
 
-	return (
-		<>
-			<BrowserRouter>
-				<div className="container-fluid">
-					<div className="row">
-						{loggedIn && <ClassesSidebar />}
-						<Routes>
-							<Route path="/" element={<Dashboard />} />
-							<Route path="/:assignmentID" element={<AssignmentDetails data={dummyUser} />} />
-							<Route path="/:assignmentID/:noteID" element={<NoteDetails />} />
-						</Routes>
-					</div>
-				</div>
-			</BrowserRouter>
-		</>
-	)
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setLoggedIn(true);
+            } else {
+                setLoggedIn(false);
+            }
+        });
+    }, []);
+
+    return (
+        <>
+            <BrowserRouter>
+                <div className="container-fluid">
+                    <div className="row">
+                        {auth.currentUser && <ClassesSidebar />}
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route
+                                path="/:assignmentID"
+                                element={<AssignmentDetails data={dummyUser} />}
+                            />
+                            <Route
+                                path="/:assignmentID/:noteID"
+                                element={<NoteDetails />}
+                            />
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
