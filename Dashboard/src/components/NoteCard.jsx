06622e2e80100d5b093ce956cf4b4b8632335d3a
EthanHaller <ttk4ey@virginia.edu>
2024-03-23 17:48:30 -0400

    gitignore

--- Dashboard/src/components/NoteCard.jsx
+++ Dashboard/src/components/NoteCard.jsx
@@ -1,21 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/NoteCard.css";
 
 const NoteCard = ({ assignmentID, note }) => {
     const navigate = useNavigate();
 
-    const handleViewNote = (noteID) => {
-        navigate(`/${assignmentID}/${noteID}`);
-    };
-
     return (
-        <div className="card note-summary-card" onClick={() => handleViewNote(note.id)}>
+        <Link to={`/${assignmentID}/${note.id}`} className="card note-summary-card">
             <div className="card-header">{note.name}</div>
             <div className="card-body">
                 {/* TODO: cut off the summary with a ... after a certain number of characters */}
                 <p className="card-text">{note.summary}</p>
             </div>
-        </div>
+        </Link>
     );
 };
 
