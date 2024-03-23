537de457fbdf58f110ddd04f0a7f25589709d32d
simonanderson16 <uhx8bu@virginia.edu>
2024-03-23 15:06:47 -0400

    notecard layout

--- Dashboard/src/components/DashboardContent.jsx
+++ Dashboard/src/components/DashboardContent.jsx
@@ -1,40 +1,44 @@
-import Citations from "./Citations"
-import NoteCard from "./NoteCard"
+import Citations from "./Citations";
+import NoteCard from "./NoteCard";
 
 const DashboardContent = ({ assignment }) => {
-	const getAllNoteUrls = (notes) => {
-		const urls = []
-		notes.forEach((note) => {
-			urls.push(note.url)
-		})
-		return urls
-	}
+    const getAllNoteUrls = (notes) => {
+        const urls = [];
+        notes.forEach((note) => {
+            urls.push(note.url);
+        });
+        return urls;
+    };
 
-	return (
-		<>
-			{assignment && (
-				<div className="col-10">
-					<h1>{assignment.name}</h1>
-					{assignment.due ? <h4>Due Date: {assignment.due}</h4> : null}
-					<h4>Notes: </h4>
-					{assignment.notes.map((note, index) => (
-						<NoteCard key={index} note={note} />
-					))}
-					<Citations urls={getAllNoteUrls(assignment.notes)} />
-					<h4>More Resources: </h4>
-					<ul>
-						{assignment.urlRecs.map((url, index) => (
-							<li key={index}>
-								<a key={index} href={url}>
-									{url}
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</>
-	)
-}
+    return (
+        <>
+            {assignment && (
+                <div className="col-10">
+                    <h1>{assignment.name}</h1>
+                    {assignment.due ? (
+                        <h4>Due Date: {assignment.due}</h4>
+                    ) : null}
+                    <h4>Notes: </h4>
+                    <div className="note-card-container">
+                        {assignment.notes.map((note, index) => (
+                            <NoteCard key={index} note={note} />
+                        ))}
+                    </div>
+                    <Citations urls={getAllNoteUrls(assignment.notes)} />
+                    <h4>More Resources: </h4>
+                    <ul>
+                        {assignment.urlRecs.map((url, index) => (
+                            <li key={index}>
+                                <a key={index} href={url}>
+                                    {url}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </>
+    );
+};
 
-export default DashboardContent
+export default DashboardContent;
--- Dashboard/src/components/NoteCard.jsx
+++ Dashboard/src/components/NoteCard.jsx
@@ -1,18 +1,20 @@
+import "../styles/NoteCard.css";
+
 const NoteCard = ({ note }) => {
     const { name, url } = note;
 
+    const handleViewNote = (noteID) => {
+        console.log(noteID);
+    };
+
     return (
-        <>
-            {Object.entries(note.keywords).map((entry, index) => (
-                <div className="card">
-                    <div className="card-header">{entry[0]}</div>
-                    <div className="card-body">
-                        <p className="card-text">{entry[1]}</p>
-                        {/* <button className="btn btn-danger">Delete</button> */}
-                    </div>
-                </div>
-            ))}
-        </>
+        <div className="card note-summary-card" onClick={() => handleViewNote(note.id)}>
+            <div className="card-header">{note.name}</div>
+            <div className="card-body">
+                {/* TODO: cut off the summary with a ... after a certain number of characters */}
+                <p className="card-text">{note.summary}</p>
+            </div>
+        </div>
     );
 };
 
