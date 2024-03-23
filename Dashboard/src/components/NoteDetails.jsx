84d70db689db8b316242daf0b52e327b3479e94f
EthanHaller <ttk4ey@virginia.edu>
2024-03-23 16:32:03 -0400

    create keyword cards

--- Dashboard/src/components/NoteDetails.jsx
+++ Dashboard/src/components/NoteDetails.jsx
@@ -1,16 +1,35 @@
 import { useParams } from "react-router-dom"
+import "../styles/NoteDetails.css"
 
 const NoteDetails = ({}) => {
 	const { noteID } = useParams()
 
 	// get note info from db
-    const data = demoResponse // from fetch
+	const data = demoResponse // from fetch
+
+	const Keywords = Object.entries(data.note.keywords).map(([keyword, definition]) => (
+		<div key={keyword} className="keyword-container">
+			<div className="keyword-card">
+				<h5 className="keyword">{keyword}</h5>
+                <p className="definition">{definition}</p>
+			</div>
+		</div>
+	))
 
 	return (
 		<>
 			{noteID && (
 				<div className="col-10">
-					<h1>{data.assignmentName} - {data.note.name}</h1>
+					<h1>
+						{data.assignmentName} - {data.note.name}
+					</h1>
+					{data.note.url && (
+						<h4>
+							Source: <a href={data.note.url}>{data.note.url}</a>
+						</h4>
+					)}
+					<p>{data.note.summary}</p>
+					<div className="keywords">{Keywords}</div>
 				</div>
 			)}
 		</>
@@ -20,15 +39,15 @@ const NoteDetails = ({}) => {
 export default NoteDetails
 
 const demoResponse = {
-    assignmentName: "Assignment1",
-    note: {
-        name: "Note1",
-        url: "http://www.google.com",
-        summary: "This is a summary for Note1",
-        keywords: {
-            "keyword 1": "definition for keyword 1",
-            "keyword 2": "definition for keyword 2",
-            "keyword 3": "definition for keyword 3",
-        },
-    }
+	assignmentName: "Assignment1",
+	note: {
+		name: "Note1",
+		url: "http://www.google.com",
+		summary: "This is a summary for Note1",
+		keywords: {
+			"keyword 1": "definition for keyword 1",
+			"keyword 2": "definition for keyword 2",
+			"keyword 3": "definition for keyword 3",
+		},
+	},
 }
