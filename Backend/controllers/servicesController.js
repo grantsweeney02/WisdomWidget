981b670ee63789a6f35b26a7ce8d01ab5c81d1a2
EthanHaller <ttk4ey@virginia.edu>
2024-03-24 00:11:26 -0400

    begin openai service

--- Backend/controllers/servicesController.js
+++ Backend/controllers/servicesController.js
@@ -1,2 +1,11 @@
-exports.openai = async (req, res) => {};
+const cheerio = require("cheerio")
+const { summarize } = require("../openai");
 
+exports.openai = async (req, res) => {
+    const { pageContent, url } = req.body
+
+    const parsedHTML = parseHTML(pageContent)
+    const summary = await summarize(parsedHTML)
+
+    res.status(200).send(summary)
+};
