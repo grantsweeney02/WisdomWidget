b9d741e85a52ab98d34d4ba620d29bc0506e1b3a
Grant Sweeney <grantsweeney02@gmail.com>
2024-03-23 21:06:55 -0400

    updated to a post

--- Backend/routes/assignments.js
+++ Backend/routes/assignments.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const assignmentsController = require("../controllers/assignmentsController");
 
-router.get("/getAssignment", assignmentsController.getAssignment);
+router.post("/getAssignment", assignmentsController.getAssignment);
 
 module.exports = router;
