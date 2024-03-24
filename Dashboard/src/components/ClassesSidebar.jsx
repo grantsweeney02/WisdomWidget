0ad1dcd3d052d23f028c4b0b96b3c8ddcf523185
EthanHaller <ttk4ey@virginia.edu>
2024-03-23 22:17:59 -0400

    attempt to fix

--- Dashboard/src/components/ClassesSidebar.jsx
+++ Dashboard/src/components/ClassesSidebar.jsx
@@ -1,159 +1,129 @@
-import { useNavigate, useParams } from "react-router-dom";
-import dummyUser from "../../dummyUser.json";
-import { auth } from "../../firebaseConfig";
-import LogoutIcon from "@mui/icons-material/Logout";
-import AddIcon from "@mui/icons-material/Add";
-import CloseIcon from "@mui/icons-material/Close";
-import CheckIcon from "@mui/icons-material/Check";
-import "../styles/classessidebar.css";
-import { useState, useContext, useEffect } from "react";
-import { UserContext } from "../App";
+import { useNavigate, useParams } from "react-router-dom"
+import dummyUser from "../../dummyUser.json"
+import { auth } from "../../firebaseConfig"
+import LogoutIcon from "@mui/icons-material/Logout"
+import AddIcon from "@mui/icons-material/Add"
+import CloseIcon from "@mui/icons-material/Close"
+import CheckIcon from "@mui/icons-material/Check"
+import "../styles/classessidebar.css"
+import { useState, useContext, useEffect } from "react"
+import { UserContext } from "../App"
 
 const ClassesSidebar = ({}) => {
-    const navigate = useNavigate();
-    const [classes, setClasses] = useState([]);
-    const [activeAssignment, setActiveAssignment] = useState(false);
-    const [isAddingClass, setIsAddingClass] = useState(false);
-    const [newClassName, setNewClassName] = useState("");
+	const navigate = useNavigate()
+	const [classes, setClasses] = useState([])
+	const [activeAssignment, setActiveAssignment] = useState(false)
+	const [isAddingClass, setIsAddingClass] = useState(false)
+	const [newClassName, setNewClassName] = useState("")
 
-    const userData = useContext(UserContext).userData;
+	const userData = useContext(UserContext).userData
 
-    useEffect(() => {
-        if (userData) {
-            setClasses(userData.classes);
-        }
-    }, [userData]);
+	useEffect(() => {
+		if (userData) {
+			setClasses(userData.classes)
+		}
+	}, [userData])
 
-    const handleAssignmentChange = (classID, assignment) => {
-        setActiveAssignment(assignment);
-        navigate(`/${classID}-${assignment.id}`);
-    };
+	const handleAssignmentChange = (classID, assignment) => {
+		setActiveAssignment(assignment)
+		navigate(`/${classID}-${assignment.id}`)
+	}
 
-    const handleLogout = async () => {
-        await auth.signOut();
-        navigate("/");
-    };
+	const handleLogout = async () => {
+		await auth.signOut()
+		navigate("/")
+	}
 
-    const handleAddClass = () => {
-        setIsAddingClass(true);
-    };
+	const handleAddClass = () => {
+		setIsAddingClass(true)
+	}
 
-    const handleCancelAddClass = () => {
-        setIsAddingClass(false);
-        setNewClassName("");
-    };
+	const handleCancelAddClass = () => {
+		setIsAddingClass(false)
+		setNewClassName("")
+	}
 
-    const handleConfirmAddClass = () => {
-        setIsAddingClass(false);
-        setNewClassName("");
-    };
+	const handleConfirmAddClass = () => {
+		setIsAddingClass(false)
+		setNewClassName("")
+	}
 
-    const ClassAccordionItems = classes.map((classData, index) => {
-        const AssignmentButtons = classData.assignments.map(
-            (assignmentData, index2) => {
-                return (
-                    <button
-                        key={"" + assignmentData.id}
-                        type="button"
-                        className={
-                            "btn btn-primary assignment-button" +
-                            (activeAssignment.id == assignmentData.id
-                                ? " active"
-                                : "")
-                        }
-                        onClick={() =>
-                            handleAssignmentChange(classData.id, assignmentData)
-                        }
-                    >
-                        {assignmentData.name}
-                    </button>
-                );
-            }
-        );
+	const ClassAccordionItems = classes ? (classes.map((classData, index) => {
+		const AssignmentButtons = classData.assignments.map((assignmentData, index2) => {
+			return (
+				<button
+					key={"" + assignmentData.id}
+					type="button"
+					className={"btn btn-primary assignment-button" + (activeAssignment.id == assignmentData.id ? " active" : "")}
+					onClick={() => handleAssignmentChange(classData.id, assignmentData)}
+				>
+					{assignmentData.name}
+				</button>
+			)
+		})
 
-        return (
-            <div key={"" + classData.id} className="accordion-item">
-                <h2 className="accordion-header">
-                    <button
-                        className="accordion-button collapsed"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target={"#collapse-" + classData.id}
-                        aria-expanded="false"
-                        aria-controls={"collapse-" + classData.id}
-                    >
-                        {classData.name}
-                    </button>
-                </h2>
-                <div
-                    id={"collapse-" + classData.id}
-                    className="accordion-collapse collapse"
-                    data-bs-parent="#classesAccordion"
-                >
-                    <div className="accordion-body">
-                        <div
-                            className="btn-group-vertical assignment-buttons"
-                            role="group"
-                            aria-label={classData.name + " Assignment Buttons"}
-                        >
-                            {AssignmentButtons}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    });
+		return (
+			<div key={"" + classData.id} className="accordion-item">
+				<h2 className="accordion-header">
+					<button
+						className="accordion-button collapsed"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target={"#collapse-" + classData.id}
+						aria-expanded="false"
+						aria-controls={"collapse-" + classData.id}
+					>
+						{classData.name}
+					</button>
+				</h2>
+				<div id={"collapse-" + classData.id} className="accordion-collapse collapse" data-bs-parent="#classesAccordion">
+					<div className="accordion-body">
+						<div className="btn-group-vertical assignment-buttons" role="group" aria-label={classData.name + " Assignment Buttons"}>
+							{AssignmentButtons}
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	})) : (
+		<p>Loading...</p>
+	)
 
-    return (
-        <nav className="classes-sidebar col-3">
-            <h2>Classes</h2>
-            <div className="accordion" id="classesAccordion">
-                {ClassAccordionItems}
-            </div>
-            {isAddingClass ? (
-                <div className="add-class-input">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter class name"
-                        value={newClassName}
-                        onChange={(e) => setNewClassName(e.target.value)}
-                    />
-                    <div className="add-class-confirms">
-                        <button
-                            type="button"
-                            className="btn"
-                            onClick={() => handleConfirmAddClass()}
-                        >
-                            <CheckIcon />
-                        </button>
-                        <button
-                            type="button"
-                            className="btn"
-                            onClick={() => handleCancelAddClass()}
-                        >
-                            <CloseIcon />
-                        </button>
-                    </div>
-                </div>
-            ) : (
-                <button
-                    className="btn add-class-button"
-                    onClick={() => handleAddClass()}
-                >
-                    <AddIcon style={{ marginInline: "0.125rem" }} />
-                    Add Class
-                </button>
-            )}
-            <button
-                type="button"
-                className="btn btn-danger sidebar-logout-button"
-                onClick={() => handleLogout()}
-            >
-                <LogoutIcon />
-            </button>
-        </nav>
-    );
-};
+	return (
+		<nav className="classes-sidebar col-3">
+			<h2>Classes</h2>
+			<div className="accordion" id="classesAccordion">
+				{ClassAccordionItems}
+			</div>
+			{isAddingClass ? (
+				<div className="add-class-input">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Enter class name"
+						value={newClassName}
+						onChange={(e) => setNewClassName(e.target.value)}
+					/>
+					<div className="add-class-confirms">
+						<button type="button" className="btn" onClick={() => handleConfirmAddClass()}>
+							<CheckIcon />
+						</button>
+						<button type="button" className="btn" onClick={() => handleCancelAddClass()}>
+							<CloseIcon />
+						</button>
+					</div>
+				</div>
+			) : (
+				<button className="btn add-class-button" onClick={() => handleAddClass()}>
+					<AddIcon style={{ marginInline: "0.125rem" }} />
+					Add Class
+				</button>
+			)}
+			<button type="button" className="btn btn-danger sidebar-logout-button" onClick={() => handleLogout()}>
+				<LogoutIcon />
+			</button>
+		</nav>
+	)
+}
 
-export default ClassesSidebar;
+export default ClassesSidebar
--- Dashboard/src/components/NoteDetails.jsx
+++ Dashboard/src/components/NoteDetails.jsx
@@ -1,167 +1,144 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
-import "../styles/NoteDetails.css";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import AddIcon from "@mui/icons-material/Add";
-import CheckIcon from "@mui/icons-material/Check";
-import CloseIcon from "@mui/icons-material/Close";
-import { useEffect, useContext } from "react";
-import { UserContext } from "../App";
-import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom"
+import { useState } from "react"
+import "../styles/NoteDetails.css"
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew"
+import AddIcon from "@mui/icons-material/Add"
+import CheckIcon from "@mui/icons-material/Check"
+import CloseIcon from "@mui/icons-material/Close"
+import { useEffect, useContext } from "react"
+import { UserContext } from "../App"
+import axios from "axios"
 
 const NoteDetails = ({}) => {
-    const { classIDassignmentID, noteID } = useParams();
-    const classID = classIDassignmentID.split("-")[0];
-    const assignmentID = classIDassignmentID.split("-")[1];
-    const navigate = useNavigate();
-    const [newKeyword, setNewKeyword] = useState("");
-    const [newDefinition, setNewDefinition] = useState("");
-    const [addingKeyword, setAddingKeyword] = useState(false);
-    const [data, setData] = useState(null); // Initial data from demoResponse
+	const { classIDassignmentID, noteID } = useParams()
+	const classID = classIDassignmentID.split("-")[0]
+	const assignmentID = classIDassignmentID.split("-")[1]
+	const navigate = useNavigate()
+	const [newKeyword, setNewKeyword] = useState("")
+	const [newDefinition, setNewDefinition] = useState("")
+	const [addingKeyword, setAddingKeyword] = useState(false)
+	const [data, setData] = useState(null) // Initial data from demoResponse
 
-    const userData = useContext(UserContext).userData;
+	const userData = useContext(UserContext).userData
 
-    useEffect(() => {
-        if (userData) {
-            const populateNote = async () => {
-                const uid = userData.uid;
-                const body = {
-                    uid: uid,
-                    classId: classID,
-                    assignmentId: assignmentID,
-                    noteId: noteID,
-                };
-                const response = await axios.post(
-                    "http://localhost:8000/notes/getNote",
-                    body
-                );
-                setData(response.data);
-            };
-            populateNote();
-        }
-    }, [userData]);
+	useEffect(() => {
+		if (userData) {
+			const populateNote = async () => {
+				const uid = userData.uid
+				const body = {
+					uid: uid,
+					classId: classID,
+					assignmentId: assignmentID,
+					noteId: noteID,
+				}
+				const response = await axios.post("http://localhost:8000/notes/getNote", body)
+				setData(response.data)
+			}
+			populateNote()
+		}
+	}, [userData])
 
-    const handleBack = () => {
-        navigate(-1);
-    };
+	const handleBack = () => {
+		navigate(-1)
+	}
 
-    const handleAddKeyword = () => {
-        setAddingKeyword(true);
-    };
+	const handleAddKeyword = () => {
+		setAddingKeyword(true)
+	}
 
-    const handleConfirmAddKeyword = () => {
-        const newData = { ...data };
-        newData.keyValuePairs[newKeyword] = newDefinition;
-        setData(newData);
-        setNewKeyword("");
-        setNewDefinition("");
-        setAddingKeyword(false);
-    };
+	const handleConfirmAddKeyword = () => {
+		const newData = { ...data }
+		newData.keyValuePairs[newKeyword] = newDefinition
+		setData(newData)
+		setNewKeyword("")
+		setNewDefinition("")
+		setAddingKeyword(false)
+	}
 
-    const handleCancelAddKeyword = () => {
-        setNewKeyword("");
-        setNewDefinition("");
-        setAddingKeyword(false);
-    };
+	const handleCancelAddKeyword = () => {
+		setNewKeyword("")
+		setNewDefinition("")
+		setAddingKeyword(false)
+	}
 
-    let Keywords = null;
+	let Keywords = null
 
-    useEffect(() => {
-        if (data) {
-            console.log("Data", data);
-            console.log("Pairs", data.keyValuePairs);
-            Keywords = Object.entries(data.keyValuePairs).map(
-                ([keyword, definition]) => (
-                    <div key={keyword} className="keyword-container">
-                        <div className="keyword-card">
-                            <h5 className="keyword">{keyword}</h5>
-                            <p className="definition">{definition}</p>
-                        </div>
-                    </div>
-                )
-            );
-        }
-    }, [data]);
+	useEffect(() => {
+		if (data) {
+			console.log("Data", data)
+			console.log("Pairs", data.keyValuePairs)
+			Keywords = Object.entries(data.keyValuePairs).map(([keyword, definition]) => (
+				<div key={keyword} className="keyword-container">
+					<div className="keyword-card">
+						<h5 className="keyword">{keyword}</h5>
+						<p className="definition">{definition}</p>
+					</div>
+				</div>
+			))
+		}
+	}, [data])
 
-    return (
-        <>
-            {data && (
-                <div className="col-9 note-details">
-                    <h1>{data.name}</h1>
-                    <button
-                        className="btn back-button"
-                        onClick={() => handleBack()}
-                    >
-                        <ArrowBackIosNewIcon />
-                    </button>
-                    <p>{data.summary}</p>
-                    <div className="keywords">
-                        {Keywords}
-                        {addingKeyword ? (
-                            <div className="keyword-add-container">
-                                <input
-                                    className="form-control keyword-input"
-                                    type="text"
-                                    placeholder="New Keyword"
-                                    value={newKeyword}
-                                    onChange={(e) =>
-                                        setNewKeyword(e.target.value)
-                                    }
-                                />
-                                <input
-                                    className="form-control definition-input"
-                                    type="text"
-                                    placeholder="Definition"
-                                    value={newDefinition}
-                                    onChange={(e) =>
-                                        setNewDefinition(e.target.value)
-                                    }
-                                />
-                                <div className="keyword-confirm-buttons">
-                                    <button
-                                        className="btn"
-                                        onClick={handleConfirmAddKeyword}
-                                    >
-                                        <CheckIcon />
-                                    </button>
-                                    <button
-                                        className="btn"
-                                        onClick={handleCancelAddKeyword}
-                                    >
-                                        <CloseIcon />
-                                    </button>
-                                </div>
-                            </div>
-                        ) : (
-                            <button
-                                className="keyword-add-container"
-                                onClick={() => handleAddKeyword()}
-                            >
-                                <div className="keyword-add-button">
-                                    <AddIcon />
-                                    Add Keyword
-                                </div>
-                            </button>
-                        )}
-                    </div>
-                    {data.url && (
-                        <h4>
-                            Source: <a href={data.url}>{data.url}</a>
-                        </h4>
-                    )}
-                    {!data.url && (
-                        <>
-                            <h4>Source:</h4>
-                            <p>None Provided</p>
-                            <button className="btn btn-primary">
-                                Add Source
-                            </button>
-                        </>
-                    )}
-                </div>
-            )}
-        </>
-    );
-};
+	return (
+		<>
+			{data && (
+				<div className="col-9 note-details">
+					<h1>{data.name}</h1>
+					<button className="btn back-button" onClick={() => handleBack()}>
+						<ArrowBackIosNewIcon />
+					</button>
+					<p>{data.summary}</p>
+					<div className="keywords">
+						{Keywords}
+						{addingKeyword ? (
+							<div className="keyword-add-container">
+								<input
+									className="form-control keyword-input"
+									type="text"
+									placeholder="New Keyword"
+									value={newKeyword}
+									onChange={(e) => setNewKeyword(e.target.value)}
+								/>
+								<input
+									className="form-control definition-input"
+									type="text"
+									placeholder="Definition"
+									value={newDefinition}
+									onChange={(e) => setNewDefinition(e.target.value)}
+								/>
+								<div className="keyword-confirm-buttons">
+									<button className="btn" onClick={handleConfirmAddKeyword}>
+										<CheckIcon />
+									</button>
+									<button className="btn" onClick={handleCancelAddKeyword}>
+										<CloseIcon />
+									</button>
+								</div>
+							</div>
+						) : (
+							<button className="keyword-add-container" onClick={() => handleAddKeyword()}>
+								<div className="keyword-add-button">
+									<AddIcon />
+									Add Keyword
+								</div>
+							</button>
+						)}
+					</div>
+					{data.url && (
+						<h4>
+							Source: <a href={data.url}>{data.url}</a>
+						</h4>
+					)}
+					{!data.url && (
+						<>
+							<h4>Source:</h4>
+							<p>None Provided</p>
+							<button className="btn btn-primary">Add Source</button>
+						</>
+					)}
+				</div>
+			)}
+		</>
+	)
+}
 
-export default NoteDetails;
+export default NoteDetails
